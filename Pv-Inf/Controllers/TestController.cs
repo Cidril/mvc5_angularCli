2dec6b9a5c5b8c460a9978ba7d38cfcda9adc36c
Alex <if15b052@technikum-wien.at>
2016-11-18 15:29:18 +0100

    TestControler_added

---
+++ Pv-Inf/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Pv_Inf.Controllers
+{
+    public class TestController : ApiController
+    {
+        public IHttpActionResult getText()
+        {
+            return Ok(new { id = -1, text = "WOOOOOOHOOOOOOO" });
+        }
+    }
+}
