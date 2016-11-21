a292fb982ed42b3285e963485091e9918d348376
Alex <if15b052@technikum-wien.at>
2016-11-21 16:42:52 +0100

    Controller added

---
+++ Pv-Inf.Common/DAL/Project.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pv_Inf.Common.DAL
+{
+    [Table("group")]
+    public class Project
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("project_id")]
+        public int Id { get; set; }
+    }
+}
---
+++ Pv-Inf/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace INF_PV_neu.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public struct LoginData
+        {
+            public string username;
+            public string password;
+        }
+
+        [HttpPost]
+        [Route("api/login")]
+        public IHttpActionResult Login([FromBody]LoginData loginData)
+        {
+            if (loginData.username == "asd" && loginData.password == "asd")
+                return Ok(true);
+            return Ok(false);
+        }
+    }
+}
